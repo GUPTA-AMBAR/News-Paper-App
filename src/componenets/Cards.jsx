34a79398e51569809cf4ed5cc8f24902ae112387
AMBAR GUPTA <154041534+GUPTA-AMBAR@users.noreply.github.com>
2024-09-30 00:09:52 -0500

    second change

--- src/componenets/Cards.jsx
+++ src/componenets/Cards.jsx
@@ -29,37 +29,3 @@ const Card=({data})=>{
     
 }
 export default Card;
-
-
-
-//style={{width: "18rem"}}
-
-
-
-// const Card = ({ data }) => {
-//     return (
-//       <div>
-//         {data.map((currentItem, index) => {
-//           if (!currentItem.urlToImage) {
-//             return null;
-//           } else {
-//             return (
-//               <div className="card" style={{ width: '18rem' }} key={index}>
-//                 <img className="card-img-top" src={currentItem.urlToImage} alt="Card image cap" />
-//                 <div className="card-body">
-//                   <h5 className="card-title">{currentItem.title}</h5>
-//                   <p className="card-text">{currentItem.description}</p>
-//                   <a href={currentItem.url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
-//                     Read More
-//                   </a>
-//                 </div>
-//               </div>
-//             );
-//           }
-//         })}
-//       </div>
-//     );
-//   };
-  
-//   export default Card;
-  
